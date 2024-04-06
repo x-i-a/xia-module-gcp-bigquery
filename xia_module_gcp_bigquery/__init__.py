1742021faa71146d0443ee2df2d8f254a5d11c4f
X-I-A <develop@x-i-a.com>
2024-04-06 17:43:11 +0200

    feat: explict module declaration

--- xia_module_gcp_bigquery/__init__.py
+++ xia_module_gcp_bigquery/__init__.py
@@ -1,9 +1,14 @@
 from xia_module_gcp_bigquery.table import Table
 from xia_module_gcp_bigquery.dataset import Dataset
 
+modules = {
+    "gcp-module-dataset": "Dataset",
+    "gcp-module-table": "Table"
+}
+
 __all__ = [
     "Table",
     "Dataset"
 ]
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
