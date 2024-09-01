aac2d7178181acdf7430a506d553096053219111
X-I-A <develop@x-i-a.com>
2024-09-01 22:43:01 +0200

    feat: billing project alignment

--- xia_module_gcp_bigquery/templates/gcp-module-dataset/module/main.tf
+++ xia_module_gcp_bigquery/templates/gcp-module-dataset/module/main.tf
@@ -24,4 +24,6 @@ resource "google_bigquery_dataset" "app_datasets" {
   friendly_name       = each.value["friendly_name"]
   location            = each.value["location"]
   delete_contents_on_destroy = each.value["delete_contents_on_destroy"]
+  default_table_expiration_ms = lookup(each.value, "default_table_expiration_ms", null)
+  default_partition_expiration_ms = lookup(each.value, "default_partition_expiration_ms", null)
 }
